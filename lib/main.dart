4579ce27066931a052b2f6fd9f6e0be684ab967e
ylose <heungeob1003@gmail.com>
2022-12-29 12:46:38 +0900

    ListView, ListTile 실습

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,8 @@ class MyApp extends StatelessWidget {
 }
 // 여기까지는 공통코드
 
+final items = List.generate(100, (i)=>i).toList();
+
 //여기부터 수정
 class MyHomePage extends StatelessWidget {
   @override
@@ -24,31 +26,29 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Stack'),
       ),
-      body: Stack (
-        children: <Widget>[
-          Container(
-            color: Colors.red,
-            width: 100,
-            height: 100,
-            padding: const EdgeInsets.all(8.0),
-            margin: const EdgeInsets.all(8.0),
+      body: ListView(
+        scrollDirection: Axis.vertical,
+        children:<Widget>[
+          ListTile(
+            leading:Icon(Icons.home),
+            title:Text('Home'),
+            trailing:Icon(Icons.navigate_next),
+            onTap:() {},
           ),
-          Container(
-            color: Colors.green,
-            width: 80,
-            height: 80,
-            padding: const EdgeInsets.all(8.0),
-            margin: const EdgeInsets.all(8.0),
+          ListTile(
+            leading:Icon(Icons.event),
+            title:Text('Event'),
+            trailing:Icon(Icons.navigate_next),
+            onTap:() {},
           ),
-          Container(
-            color: Colors.blue,
-            width: 60,
-            height: 60,
-            padding: const EdgeInsets.all(8.0),
-            margin: const EdgeInsets.all(8.0),
-          ),
-        ],
-      ),
+          ListTile(
+            leading:Icon(Icons.camera),
+            title:Text('Camera'),
+            trailing:Icon(Icons.navigate_next),
+            onTap:() {},
+          )
+        ]
+      )
     );
   }
 }
