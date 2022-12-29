4c2e8e37d207fa87293e398992a6a2f491e25097
ylose <heungeob1003@gmail.com>
2022-12-29 13:19:41 +0900

    AppBar, TabBar, Tab, TabBarView 실습

--- lib/main.dart
+++ lib/main.dart
@@ -22,23 +22,26 @@ final items = List.generate(100, (i)=>i).toList();
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Stack'),
-      ),
-      body: PageView(
-        children:<Widget>[
-          Container(
-            color:Colors.red,
-          ),
-          Container(
-            color:Colors.green,
-          ),
-          Container(
-            color:Colors.blue,
-          ),
-        ]
-      )
-    );
+    return DefaultTabController(
+      length:3,
+        child: Scaffold(
+            appBar: AppBar(
+              title: Text('Tab'),
+              bottom:TabBar(
+                tabs:<Widget>[
+                  Tab(icon:Icon(Icons.tag_faces)),
+                  Tab(text:'메뉴2'),
+                  Tab(icon:Icon(Icons.info),text:'메뉴'),
+                ],
+              ),
+            ),
+            body:TabBarView(
+              children:<Widget>[
+                Container(color:Colors.yellow,),
+                Container(color:Colors.orange,),
+                Container(color:Colors.red,),
+              ]),
+            ),
+        );
   }
 }
